de7b9b055928ac9bf7d11eeaa8589260591959ef
duhaime <douglas.duhaime@gmail.com>
2017-12-01 14:51:11 -0500

    animate 404 page - closes #263

--- assets/js/four-oh-four.js
+++ assets/js/four-oh-four.js
@@ -1,19 +1,31 @@
 import * as d3 from 'd3';
 
 (function(d3) {
+  window.d3 = d3;
   if (!window.location.href.includes('404.html')) return;
   var rects = d3.select('body').selectAll('rect'),
       colors = ['#1981AA', '#FFC426', '#fff', '#000'];
 
-  setInterval(update, 5)
+  d3.select('body').selectAll('rect').transition()
+    .delay(function(d) {
+      return (d3.select(this).attr('x')/5) + (d3.select(this).attr('y') * 4);
+    })
+    .attr('fill', getRandomColor())
+
+  setTimeout(function() {
+    setInterval(update, 5)
+  }, 10)
 
   function update() {
     var val = parseInt( Math.random() * rects.size() );
-    var color = colors[ parseInt( Math.random() * colors.length ) ];
     rects.each(function(d, i) {
       if (i === val) {
-        d3.select(this).attr('fill', color)
+        d3.select(this).attr('fill', getRandomColor())
       }
     })
+  }
+
+  function getRandomColor() {
+    return colors[ parseInt( Math.random() * colors.length ) ];
   }
 })(d3);
