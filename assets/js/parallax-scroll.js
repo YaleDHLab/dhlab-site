32c227a4a23619704197ae5f0cd7d2055be5929a
duhaime <douglas.duhaime@gmail.com>
2018-02-15 10:53:13 -0500

    ie scrolly

--- assets/js/parallax-scroll.js
+++ assets/js/parallax-scroll.js
@@ -7,7 +7,7 @@
   window.addEventListener('resize', handleScroll);
 
   function handleScroll() {
-    const y = window.scrollY;
+    const y = window.scrollY || window.pageYOffset;
     hero.style.backgroundPositionY = window.innerWidth < 1000
       ? 0
       : (-(y*1.5) - 120) + 'px';
