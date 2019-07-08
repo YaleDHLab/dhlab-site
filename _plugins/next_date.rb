5d16d503329258abecc1670cb5b88c348a86581c
Jake Kara <jake@jakekara.com>
2019-07-08 10:04:36 -0400

    Made next_date plugin use >= instead of > when classifying date as upcoming

--- _plugins/next_date.rb
+++ _plugins/next_date.rb
@@ -10,14 +10,14 @@ Jekyll::Hooks.register :events, :pre_render do |document|
             dt = date
             puts "next_date plugin: Overriding document date from #{document.data['date']} to #{dt}"
             document.data['date'] = dt.to_time
-            if dt > today
+            if dt >= today
                 break
             end
         end
     elsif document.data.key?('date')
         document.data['date'] = document.data['date']
     else 
-        document.data['next_date'] = today + 100
+        # document.data['next_date'] = today + 100
     end
   
   end
