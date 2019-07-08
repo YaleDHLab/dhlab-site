21ac078b78ec3f9ba7334c98f8c4a8a4acdafd9d
Jake Kara <jake@jakekara.com>
2019-07-08 09:29:52 -0400

    Using a plugin approach instead

---
+++ _plugins/next_date.rb
@@ -0,0 +1,23 @@
+
+require 'date'
+
+Jekyll::Hooks.register :events, :pre_render do |document|
+
+    today = Date.today
+
+    if document.data.key?('dates')
+        document.data['dates'].each do |date|
+            dt = date
+            puts "next_date plugin: Overriding document date from #{document.data['date']} to #{dt}"
+            document.data['date'] = dt.to_time
+            if dt > today
+                break
+            end
+        end
+    elsif document.data.key?('date')
+        document.data['date'] = document.data['date']
+    else 
+        document.data['next_date'] = today + 100
+    end
+  
+  end
