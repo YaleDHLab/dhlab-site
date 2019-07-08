10b8740735c19d25db54ffce8ff62eadce0e871a
Jake Kara <jake@jakekara.com>
2019-07-08 10:16:19 -0400

    Added more robust logging info during build

--- _plugins/next_date.rb
+++ _plugins/next_date.rb
@@ -6,9 +6,8 @@ Jekyll::Hooks.register :events, :pre_render do |document|
     today = Date.today
 
     if document.data.key?('dates')
-        document.data['dates'].each do |date|
-            dt = date
-            puts "next_date plugin: Overriding document date from #{document.data['date']} to #{dt}"
+        document.data['dates'].each do |dt|
+            puts "next_date plugin: Overriding date field in '#{document.basename}' from #{document.data['date']} to #{dt.to_time}"
             document.data['date'] = dt.to_time
             if dt >= today
                 break
