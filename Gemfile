b25ab82856ac92fc142439233c0e993e8b1f9b22
duhaime <douglas.duhaime@gmail.com>
2019-09-09 12:03:45 -0400

    add static files

--- Gemfile
+++
@@ -1,7 +0,0 @@
-source 'https://rubygems.org'
-gem 'jekyll', '>= 3.6.3'
-gem 'minitest'
-
-group :jekyll_plugins do
-  gem 'octopress-minify-html'
-end
--- _plugins/index.rb
+++
@@ -1,9 +0,0 @@
-##
-# Index site content whenever the site builds
-##
-
-Jekyll::Hooks.register :site, :post_write do |site|
-  if site.config['create_index']
-    system('npm run index')
-  end
-end
