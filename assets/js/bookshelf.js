dece87e9d6c7ab5e391869a896119bfd9529c720
Jake Kara <jake@jakekara.com>
2021-04-13 14:14:34 -0400

    Add prev/next buttons

--- assets/js/bookshelf.js
+++ assets/js/bookshelf.js
@@ -1,28 +1,45 @@
-import { tns } from "tiny-slider/src/tiny-slider";
+import { tns } from 'tiny-slider/src/tiny-slider'
+;(() => {
+    const bookshelves = document.getElementsByClassName('shelf')
 
-(() => {
-  const bookshelves = document.getElementsByClassName("shelf-slider");
-  if (bookshelves.length < 1) {
-    console.log("No bookshelves to create");
-    return;
-  }
+    if (bookshelves.length < 1) {
+        console.log('No bookshelves to create')
+        return
+    }
 
-  Array.from(bookshelves).forEach(bookshelf => {
+    Array.from(bookshelves).forEach((bookshelf) => {
+        const container = bookshelf
+            .getElementsByClassName('shelf-slider')
+            .item(0)
+        const controlsContainer = bookshelf
+            .getElementsByClassName('shelf-controls')
+            .item(0)
 
-      const slider = new tns({
-        container: bookshelf,
-        autoWidth: true,
-        // autoHeight: true,
-        gutter: 10,
-        slideBy: 'page',
-        // autoplay: true,
-        nav: true,
-        navPosition: "bottom",
-        nextButton: false,
-        prevButton: false,
-        controls: false
-      });
-
-  });
-
-})();
+        const slider = new tns({
+            container: container,
+            autoWidth: true,
+            gutter: 5,
+            // slideBy: 'page',
+            // autoplay: true,
+            nav: true,
+            navPosition: 'bottom',
+            // controls: false,
+            // nextButton: nextButton,
+            // prevButton: "<<",
+            controlsContainer: controlsContainer,
+            lazyload: true,
+            loop: false,
+            responsive: {
+                0: {
+                    edgePadding: 10,
+                },
+                640: {
+                    edgePadding: 20,
+                },
+                700: {
+                    edgePadding: 0,
+                },
+            },
+        })
+    })
+})()
