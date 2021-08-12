e7711cdf5dff139324ecafdfb43f96636335cd48
duhaime <douglas.duhaime@gmail.com>
2021-08-12 12:01:35 -0400

    remove unneeded hrs and use css to rotate the header cube

--- assets/js/header-animation.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  function rotate() {
-    var container = document.querySelector('.site-brand');
-    var block = document.querySelector('#rotating-block');
-    container.style.overflow = 'visible';
-    block.className = 'rotated';
-  }
-  if (!window.document.documentMode) setTimeout(rotate, 1800);
-})()
